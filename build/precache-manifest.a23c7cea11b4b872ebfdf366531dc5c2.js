3bacc0e21d26c6854bde769b71fc5e0f446632d2
VadymYakovets <vad.yakovets@gmail.com>
2019-10-30 09:04:08 +0200

    first REact  HW

---
+++ build/precache-manifest.a23c7cea11b4b872ebfdf366531dc5c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db5b514966a0418f776690c74a4f91f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b630083f20175edfb4a",
+    "url": "/static/css/main.b565b978.chunk.css"
+  },
+  {
+    "revision": "c9563366c89b046a3ab0",
+    "url": "/static/js/2.b100127d.chunk.js"
+  },
+  {
+    "revision": "6b630083f20175edfb4a",
+    "url": "/static/js/main.e0e8a90c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9203469f750b4284c62aa49f9496cb0",
+    "url": "/static/media/lato-v15-latin-700.f9203469.eot"
+  },
+  {
+    "revision": "056a373d808ada4f12a2d6538e88823c",
+    "url": "/static/media/lato-v15-latin-regular.056a373d.eot"
+  }
+]);
